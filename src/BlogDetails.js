bffb3c99f787da49c9a0e15a86dee82cd4a0bcd8
azrulwm <azrulazmi.wm@gmail.com>
2022-10-06 13:24:44 +0800

    creating the delete process for blogdetails

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
@@ -8,6 +8,15 @@ const BlogDetails = () => {
     error,
     isPending,
   } = useFetch("http://localhost:3000/blogs/" + id);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    fetch("http://localhost:8000/blogs/" + blog.id, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
 
   console.log(blog, error, isPending);
 
@@ -20,6 +29,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onclick={handleClick}>Delete</button>
         </article>
       )}
     </div>
